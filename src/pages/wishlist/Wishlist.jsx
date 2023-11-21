3442e7d08a4828aba1e595baa0df4f4d61724fc2
TRY ⠀⠀⠀⠀⠀ <tagirramaz727@gmail.com>
2023-11-21 14:10:35 +0300

    5

--- src/pages/wishlist/Wishlist.jsx
+++ src/pages/wishlist/Wishlist.jsx
@@ -99,22 +99,6 @@ const Wishlist = () => {
                                             <img src={thing.img} alt={thing.title} />
                                             <Link>{thing.title}</Link>
                                         </div>
-                    
-                                        <div className="cart_thing__quantity">
-                                            <button>
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                                </svg>
-                                            </button>
-
-                                            <span>{thing.quantity}</span>
-                    
-                                            <button>
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                                </svg>
-                                            </button>
-                                        </div>
                                                             
                                         <div className="cart_thing__price_delete">
                                             <span>{thing.price} р.</span>
@@ -131,15 +115,7 @@ const Wishlist = () => {
                         </div>
 
                         <div className="container__total_price">
-                            <div>
-                                <img src="calculator.png" alt="" />
-                            </div>
-                
-                            <div>
-                                <span>ИТОГО: 15534 р.</span>
-                                <button>Рассчитать</button>
-                                <button onClick={wishlistClear}>Очистить</button>
-                            </div>
+                            <button onClick={wishlistClear}>Очистить</button>
                         </div>
                     </div>
                 
